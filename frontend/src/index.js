40a435ae49db64652fedbb2cfe4d19b34af38862
Amanyadav1901 <amanyadav_co20b2_51@dtu.ac.in>
2023-05-03 19:55:48 +0530

    updates 03.05.23

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,9 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      {/*Browser router from react-router-dom */}
       <ChakraProvider>
-        {/* chakra ui provider */}
         <App />
       </ChakraProvider>
     </BrowserRouter>
