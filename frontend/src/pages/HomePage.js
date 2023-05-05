cf4e4c89af0a33c43bc519b2ce4d11d488740830
Amanyadav1901 <amanyadav_co20b2_51@dtu.ac.in>
2023-05-05 21:19:27 +0530

    updates 05.05.23

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,7 +1,22 @@
 import React from "react";
-
+import { Box, Container, Text } from "@chakra-ui/react";
 const HomePage = () => {
-  return <div>Home</div>;
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg={"White"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl">Bubble Chat</Text>
+      </Box>
+    </Container>
+  );
 };
 
 export default HomePage;
