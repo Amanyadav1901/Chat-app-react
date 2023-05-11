67208de5af1e8ac4b6cb9e2c3607c87a06d94687
Amanyadav1901 <amanyadav_co20b2_51@dtu.ac.in>
2023-05-11 10:03:38 +0530

    Updates 11.05.23

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,10 +1,38 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../Models/userModel");
 
-const registerUser = asyncHandler(async () => {
+const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password, pic } = req.body;
 
   if (!name || !email || !password) {
     res.status(400);
     throw new Error("Please enter all the fields");
   }
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User Already Exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Failed to create New User");
+  }
 });
+
+module.exports = { registerUser };
