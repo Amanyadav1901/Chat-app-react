03bb7953f0f06e07b6984f7f696d634ec025e9db
Amanyadav1901 <amanyadav_co20b2_51@dtu.ac.in>
2023-05-14 21:31:28 +0530

    updates 14.10.23

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleWares/errorMiddleware");
 
 dotenv.config();
 connectDB();
@@ -14,15 +16,11 @@ app.get("/", (req, res) => {
   res.send("API is Running");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
+app.use("/api/user", userRoutes);
 
-app.get("/api/chat/:id", (req, res) => {
-  const singleChat = chats.find((c) => c._id === req.params.id);
-  res.send(singleChat);
-});
+app.use(notFound);
+app.use(errorHandler);
 
-const PORT = process.env.PORT || 3100;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`server started on PORT ${PORT}`.yellow.bold));
